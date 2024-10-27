51f4eb9f754553e19c461e1c20e6c304c6140ae4
Moutig <moutig-tan@proton.me>
2024-10-27 18:54:48 +0100

    it works !

--- circle_1/get_next_line/get_next_line_bonus.c
+++ circle_1/get_next_line/get_next_line_bonus.c
@@ -16,20 +16,17 @@
 # define BUFFER_SIZE 4096
 #endif
 
-int	find_newline(t_list *list, t_newline *line)
-{
+int	find_newline(t_list *list, t_newline *line) {
 	int	i;
 
 	if (!list)
 		return (0);
 	line->size = 0;
 	line->buffers = 0;
-	while (list)
-	{
+	while (list) {
 		i = 0;
 		line->buffers++;
-		while (list->buffer[i] && i < BUFFER_SIZE)
-		{
+		while (list->buffer[i] && i < BUFFER_SIZE) {
 			if (list->buffer[i] == '\n')
 				return (1);
 			i++;
@@ -40,44 +37,36 @@ int	find_newline(t_list *list, t_newline *line)
 	return (0);
 }
 
-void	create_list(t_list **list, int fd, t_newline *line)
-{
+void	create_list(t_list **list, int fd, t_newline *line) {
 	char	*buffer;
 	int		size;
 
-	while (!find_newline(list[fd], line))
-	{
+	while (!find_newline(list[fd], line)) {
 		buffer = malloc(BUFFER_SIZE + 1);
 		if (!buffer)
-			return ;
+			return;
 		size = read(fd, buffer, BUFFER_SIZE);
-		if (!size)
-		{
+		if (size <= 0) {
 			free(buffer);
-			return ;
+			return;
 		}
 		buffer[size] = '\0';
 		ft_lstadd_back(list, buffer, fd);
 	}
 }
 
-char	*extract_line(t_list *list, t_newline *line)
-{
+char	*extract_line(t_list *list, t_newline *line) {
 	char	*char_line;
-	int		i;
-	int		k;
+	int		i, k;
 
-	char_line = malloc(sizeof(char) * line->size + 2);
+	char_line = malloc(sizeof(char) * (line->size + 2));
 	if (!char_line || !list)
 		return (NULL);
 	k = 0;
-	while (list)
-	{
+	while (list) {
 		i = 0;
-		while (list->buffer[i])
-		{
-			if (list->buffer[i] == '\n')
-			{
+		while (list->buffer[i]) {
+			if (list->buffer[i] == '\n') {
 				char_line[k++] = '\n';
 				char_line[k] = '\0';
 				return (char_line);
@@ -90,14 +79,10 @@ char	*extract_line(t_list *list, t_newline *line)
 	return (char_line);
 }
 
-void	dealloc(t_list **list, t_list *clean_node, char *buf)
-{
+void	dealloc(t_list **list, t_list *clean_node, char *buf) {
 	t_list	*tmp;
 
-	if (NULL == *list)
-		return ;
-	while (*list)
-	{
+	while (*list) {
 		tmp = (*list)->next;
 		free((*list)->buffer);
 		free(*list);
@@ -106,30 +91,27 @@ void	dealloc(t_list **list, t_list *clean_node, char *buf)
 	*list = NULL;
 	if (clean_node->buffer[0])
 		*list = clean_node;
-	else
-	{
+	else {
 		free(buf);
 		free(clean_node);
 	}
 }
 
-void	polish_list(t_list **list)
-{
+void	polish_list(t_list **list) {
 	t_list	*last_node;
 	t_list	*clean_node;
-	int		i;
-	int		k;
+	int		i, k;
 	char	*buf;
 
 	buf = malloc(BUFFER_SIZE + 1);
 	clean_node = malloc(sizeof(t_list));
-	if (NULL == buf || NULL == clean_node)
-		return ;
+	if (!buf || !clean_node)
+		return;
 	last_node = ft_lstlast(*list);
 	i = 0;
 	k = 0;
 	while (last_node->buffer[i] && last_node->buffer[i] != '\n')
-		++i;
+		i++;
 	while (last_node->buffer[i] && last_node->buffer[++i])
 		buf[k++] = last_node->buffer[i];
 	buf[k] = '\0';
@@ -138,30 +120,28 @@ void	polish_list(t_list **list)
 	dealloc(list, clean_node, buf);
 }
 
-char	*get_next_line(int fd)
-{
+char	*get_next_line(int fd) {
 	static t_list	*list[4096];
 	char			*next_line;
 	t_newline		*line;
 
-	if (fd < 0 || fd > 4095 || BUFFER_SIZE <= 0 || read(fd, &next_line, 0) < 0)
+	if (fd < 0 || fd > 4095 || BUFFER_SIZE <= 0)
 		return (NULL);
 
-	// Initialiser line
 	line = malloc(sizeof(t_newline));
 	if (!line)
 		return (NULL);
-
+	line->size = 0;
+	line->buffers = 0;
 
 	create_list(list, fd, line);
-	if (!list[fd])
-	{
+	if (!list[fd]) {
 		free(line);
 		return (NULL);
 	}
 	next_line = extract_line(list[fd], line);
 	polish_list(&list[fd]);
 
-	free(line); // Libérer line après utilisation
+	free(line);
 	return (next_line);
 }
